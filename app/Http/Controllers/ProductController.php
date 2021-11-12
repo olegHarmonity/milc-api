0c7337b4fd5f17c1eae03609ff17b82920a97f08
i.tricic <i.tricic@devla.dev>
2021-11-12 14:42:49 +0100

    update GET /products route

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,12 +13,29 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Throwable;
 use \Gate;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return new CollectionResource(Product::all());
+        $products = Product::query()->with(
+            'content_type:id,name',
+            'genres:id,name',
+            'available_formats:id,name',
+            'marketing_assets:id,key_artwork_id',
+            'marketing_assets.key_artwork:id,image_name,image_url'
+        );
+
+        if ($oid = $request->input('organisation_id')) {
+            $products->where('organisation_id', $oid);
+        }
+
+        $products = $products->get([
+            'id', 'title', 'synopsis', 'runtime', 'content_type_id', 'marketing_assets_id'
+        ]);
+
+        return CollectionResource::make($products);
     }
 
     public function show(int $id)
@@ -39,7 +56,6 @@ class ProductController extends Controller
             return (new ProductResource($product))
                 ->response()
                 ->setStatusCode(Response::HTTP_CREATED);
-
         } catch (Throwable $e) {
             DB::rollback();
             throw new BadRequestHttpException($e->getMessage());
@@ -56,7 +72,6 @@ class ProductController extends Controller
             return (new ProductResource($product))
                 ->response()
                 ->setStatusCode(Response::HTTP_OK);
-
         } catch (Throwable $e) {
             DB::rollback();
             throw new BadRequestHttpException($e->getMessage());
