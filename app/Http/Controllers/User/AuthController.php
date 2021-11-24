14ff01005880ca3d07e7b48eaa9be9d8c16f9854
danijelaaaa <d.mikulicic@devla.dev>
2021-11-24 14:47:36 +0100

    message

--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -124,7 +124,7 @@ class AuthController extends Controller
 
         return response()->json([
             'success' => true,
-            'message' => 'You have successfully verified your email address.'
+            'message' => 'Verification link has been sent to your email.'
         ]);
     }
 
