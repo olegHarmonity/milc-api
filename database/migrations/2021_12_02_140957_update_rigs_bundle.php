79ca76340e5fb50d81f9669bfb0713f7154b9b96
danijelaaaa <d.mikulicic@devla.dev>
2021-12-03 11:25:57 +0100

    migration

--- database/migrations/2021_12_02_140957_update_rigs_bundle.php
+++ database/migrations/2021_12_02_140957_update_rigs_bundle.php
@@ -1,19 +1,26 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class UpdateRigsBundle extends Migration
 {
-    
+
     public function up()
     {
-        Schema::table('rights_bundles', function (Blueprint $table) {
-            $table->foreignId('product_id')->nullable()->unsigned()->references('id')->on('products')->cascadeOnDelete()->cascadeOnUpdate();
-        });
+        if (!Schema::hasColumn('rights_bundles', 'product_id')) {
+            Schema::table('rights_bundles', function (Blueprint $table) {
+                $table->foreignId('product_id')
+                    ->nullable()
+                    ->unsigned()
+                    ->references('id')
+                    ->on('products')
+                    ->cascadeOnDelete()
+                    ->cascadeOnUpdate();
+            });
+        }
     }
-    
+
     public function down()
     {
         Schema::table('rights_bundles', function (Blueprint $table) {
