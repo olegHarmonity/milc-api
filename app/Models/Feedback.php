95941aff8b735ffd3f3bfa0ffead6130ea39e2a8
Ismar Tricic <i.tricic@devla.dev>
2021-12-08 20:55:17 +0000

    add relation to user

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -2,16 +2,22 @@
 
 namespace App\Models;
 
+use App\Traits\FormattedTimestamps;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
-    use HasFactory;
+    use HasFactory, FormattedTimestamps;
 
     protected $fillable = [
         'user_id',
         'type',
         'content',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
