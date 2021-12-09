d9e1825ff0a7fae71985b84d8abace2bb8d7aade
LennardArand <lennard.arand@plutus.media>
2021-12-09 12:45:49 +0100

    Update on feedback status

---
+++ database/migrations/2021_12_09_114318_add_status_column_to_feedback_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusColumnToFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('feedback', function (Blueprint $table) {
+            $table->string('status')->nullable()->after('user_id');
+            $table->boolean('is_archived')->default(0)->nullable()->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('feedback', function (Blueprint $table) {
+            $table->dropColumn('status');
+            $table->dropColumn('is_archived');
+        });
+    }
+}
