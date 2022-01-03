31a008383b136353c847176ec2bc628a01afe9ce
danijelaaaa <d.mikulicic@devla.dev>
2022-01-03 14:02:22 +0100

    added rights-bundles create route

---
+++ app/DataTransformer/Product/CreateRightsBundlesDataTransformer.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\DataTransformer\Product;
+
+use App\Models\Money;
+use App\Models\RightsBundle;
+use Illuminate\Support\Facades\DB;
+use App\Models\Product;
+use App\Util\NotificationCategories;
+use Database\Factories\NotificationFactory;
+
+class CreateRightsBundlesDataTransformer
+{
+
+    public static function transformData($arrayRequest, Product $product)
+    {
+        DB::beginTransaction();
+
+        if (isset($arrayRequest['rights_information'])) {
+            $rightsBundlesRightsInfoRequest = $arrayRequest['rights_information'];
+            unset($arrayRequest['rights_information']);
+        }
+
+        $priceRequest = [];
+        if (isset($arrayRequest['price'])) {
+            $priceRequest = $arrayRequest['price'];
+            unset($arrayRequest['price']);
+        }
+
+        if (isset($priceRequest['id'])) {
+            $price = Money::findOrFail($priceRequest['id']);
+            $price->update($priceRequest);
+        } else {
+            $price = Money::create($priceRequest);
+        }
+
+        $price->save();
+
+        $arrayRequest['price_id'] = $price->id;
+
+        $bundleRight = RightsBundle::create($arrayRequest);
+
+        $bundleRight->save();
+
+        if (isset($rightsBundlesRightsInfoRequest)) {
+            $bundleRight->bundle_rights_information()->detach();
+
+            foreach ($rightsBundlesRightsInfoRequest as $rightsBundlesRightInfoRequest) {
+                $bundleRight->bundle_rights_information()->attach($rightsBundlesRightInfoRequest);
+            }
+        }
+
+        $bundleRight->save();
+
+        $product->rights_bundles()->attach($bundleRight->id);
+        $product->save();
+
+        if ($bundleRight->buyer_id) {
+            NotificationFactory::createNotification("Custom rights bundle created for you", "A custom rights bundle was created for your organisation for product " . $product->title . ". To see the custom bundle, go to product view page.", NotificationCategories::$OTHER, $bundleRight->buyer_id);
+        }
+        
+        DB::commit();
+
+        return $product;
+    }
+}
+
