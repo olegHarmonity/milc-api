0f3b91d2f56a888fbd5e0e8005627da01bb41b33
danijelaaaa <d.mikulicic@devla.dev>
2022-01-05 11:38:36 +0100

    test

---
+++ database/migrations/2022_01_05_113722_update_rights_test.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateRightsTest extends Migration
+{
+       public function up()
+    {
+        Schema::table('rights_bundles', function (Blueprint $table) {
+            
+            if (!Schema::hasColumn('rights_bundles', 'product_id')) {
+                Schema::table('rights_bundles', function (Blueprint $table) {
+                    $table->integer('product_id')
+                    ->nullable();
+                });
+            }
+            
+            Schema::table('rights_bundles', function (Blueprint $table) {
+                $table->foreignId('product_id')
+                ->change()
+                ->nullable()
+                ->unsigned()
+                ->references('id')
+                ->on('products')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            });
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('rights_bundles', function (Blueprint $table) {
+            $table->dropColumn('product_id');
+        });
+    }
+}
